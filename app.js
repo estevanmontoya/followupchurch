4a02ffd6c24733b44bcd70a95ebaaea90b207cc4
estevanmontoya <montoya.estevan@gmail.com>
2017-05-06 12:07:11 -0600

    Added New Contact route & styled contact list page

--- app.js
+++ app.js
@@ -14,24 +14,35 @@ app.set('views', __dirname + '/views');
 app.use(express.static('public'));
 
 // routes
+// index
 app.get('/', (req, res) => {
   res.render('index', {
     title: 'FollowUp.Church | Follow up software to help churches keep track of new contacts and converts.'
   });
 });
 
+// login
 app.get('/login', (req, res) => {
   res.render('login', {
     title: 'FollowUp.Church | Login to your FollowUp.Church account.'
   });
 });
 
+// create account
 app.get('/create', (req, res) => {
   res.render('create', {
     title: 'FollowUp.Church | Create your FollowUp.Church account.'
   });
 });
 
+// new contact
+app.get('/new', (req,res) => {
+  res.render('new', {
+    title: 'Create new contact | FollowUp.Church'
+  });
+});
+
+// contact list
 app.get('/contacts', (req, res) => {
   res.render('contacts', {
     title: 'List of contacts | FollowUp.Church'
